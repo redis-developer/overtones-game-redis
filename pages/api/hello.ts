132af9451e1466ff9ce8e96bc2e33fd617794770
Clemens Westrup <clemensw@posteo.de>
2021-05-10 19:35:57 +0300

    Add rank and score from redis to home screen

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,5 +1,7 @@
 import connectToDb from "lib/mongodb";
 import { auth } from "lib/auth-middleware";
+import redis from "lib/redis"
+import { Leaderboard } from 'redis-rank';
 
 export default async function handler(req, res) {
   const db = await connectToDb();
@@ -9,8 +11,19 @@ export default async function handler(req, res) {
     return res.status(401).json({ unauthorized: true });
   }
 
+  const lb = new Leaderboard(redis, 'lb:example', {
+    sortPolicy: 'high-to-low',
+    updatePolicy: 'replace'
+  });
+
+  const lastScore = await lb.score(user._id)
+  const lastRank = await lb.rank(user._id)
+  const totalPlayers = await lb.count()
+
   return res.status(200).send({
     username: user.username,
-    lastHighScore: user.latestHighScore || 0,
+    lastHighScore: lastScore || 0,
+    lastRank: lastRank || 0,
+    totalPlayers: totalPlayers || 0
   });
 }
