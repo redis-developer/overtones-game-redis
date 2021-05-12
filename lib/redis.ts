e533ff49f49afb61c30cc057cf68678f73e5f71d
Clemens Westrup <clemensw@posteo.de>
2021-05-12 08:04:00 +0300

    Add username and password to Redis setup, add .env.example

--- lib/redis.ts
+++ lib/redis.ts
@@ -1,8 +1,24 @@
 import Redis from "ioredis";
 
-const host = process.env.REDIS_HOST
-const port = parseInt(process.env.REDIS_PORT)
+const host = process.env.REDIS_HOST || "localhost"
+const port = parseInt(process.env.REDIS_PORT) || 6379
+const user = process.env.REDIS_USER || ""
+const password = process.env.REDIS_PASSWORD || ""
 
-const client = new Redis({host: host, port: port});
-
-export default client;
+const client  = () => {
+    if (user != "" && password != "") {
+        return new Redis({
+            host: host, 
+            port: port, 
+            username: user,
+            password: password,
+        });
+    } else {
+        return new Redis({
+            host: host, 
+            port: port
+        });
+    }
+}
+    
+export default client();
