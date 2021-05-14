28dc604d3f0336476d28d65907a804152b49fd90
David Floegel <mail@davidfloegel.com>
2021-05-14 13:50:48 +0100

    fix: redisjson connection detials?

--- lib/redis.ts
+++ lib/redis.ts
@@ -1,29 +1,29 @@
 import Redis from "ioredis";
 import Rejson from "iorejson";
 
-const host = process.env.REDIS_HOST || "localhost"
-const port = parseInt(process.env.REDIS_PORT) || 6379
-const user = process.env.REDIS_USER || ""
-const password = process.env.REDIS_PASSWORD || ""
+const host = process.env.REDIS_HOST || "localhost";
+const port = parseInt(process.env.REDIS_PORT) || 6379;
+const user = process.env.REDIS_USER || "";
+const password = process.env.REDIS_PASSWORD || "";
 
-const client  = () => {
-    if (password) {
-        return new Redis({
-            host: host, 
-            port: port, 
-            username: user,
-            password: password,
-        });
-    } else {
-        return new Redis({
-            host: host, 
-            port: port
-        });
+const connectionDetails = password
+  ? {
+      host: host,
+      port: port,
+      username: user,
+      password: password,
     }
-}
+  : {
+      host: host,
+      port: port,
+    };
 
-const rejsonClient = new Rejson();
-rejsonClient.connect()
+const client = () => {
+  return new Redis(connectionDetails);
+};
+
+const rejsonClient = new Rejson(connectionDetails);
+rejsonClient.connect();
 
 export const redis = client();
 export const redisjson = rejsonClient;
